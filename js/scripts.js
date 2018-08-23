14a323a16c0beed5dbae033be14f5b5ef9fea41c
Luke Vandekieft and Maly Phongsavanh <luke@>
2018-08-23 16:37:20 -0700

    Add two dice option for both play modes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
 // business logic
 
-//SHARED LOGIC
 Scores = [];
 currentPlayer = [];
 diceLog = [];
+thrownDice = [];
 var turnScore = 0;
 
 function Turn(name, turnScore, totalScore) {
 this.playerName = name;
 this.turnScore = turnScore;
 this.totalScore = totalScore;
-}
+};
+
+// Turn.prototype.DetailedScore = function() {
+//   if (Scores.length < 2) {
+//     return 0;
+//   } else {
+//   response = "Points remaining to win: " + toString((100 - this.totalScore));
+//   return response;
+//   }
+// };
 
 var activePlayer = function() {
   if (Scores.length % 2 === 0 || Scores.length === 0) {
@@ -18,7 +27,7 @@ var activePlayer = function() {
   } else {
     return currentPlayer[1];
   }
-}
+};
 
 var player1Score = function () {
   if (Scores.length === 0) {
@@ -28,7 +37,7 @@ var player1Score = function () {
   } else {
     return Scores[Scores.length - 2].totalScore;
   }
-}
+};
 
 var player2Score = function () {
   if (Scores.length < 2) {
@@ -38,7 +47,7 @@ var player2Score = function () {
   } else {
     return Scores[Scores.length - 2].totalScore;
   }
-}
+};
 
 var lastTotalScore = function() {
     if (Scores.length < 2) {
@@ -47,7 +56,7 @@ var lastTotalScore = function() {
     currentTotal = Scores[Scores.length - 2].totalScore;
     return currentTotal;
     }
-}
+};
 
 var dice = function() {
   var roll = Math.floor((Math.random() *6) +1);
@@ -64,7 +73,7 @@ var endTurn = function () {
     displayScores();
     playerTurnMessage(activePlayer());
     turnScore = 0;
-}
+};
 
 //TWO PLAYER LOGIC
 var rollDice = function(diceRoll) {
@@ -75,43 +84,62 @@ var rollDice = function(diceRoll) {
   } else {
     turnScore = turnScore + diceRoll;
   }
-}
+};
 
-var hold = function() {
-    endTurn();
-    clearDice();
-
-  }
-
-//COMPUTER LOGIC
-var rollDiceComputer = function(diceRoll) {
+var roll2Dice = function(diceRoll1, diceRoll2) {
   hideOne();
-  if (diceRoll === 1) {
+  if (diceRoll1 === 1 || diceRoll2 === 1) {
     turnScore = 0;
     endTurn();
-    showDice();
+  } else {
+    turnScore = turnScore + diceRoll1 + diceRoll2;
+  }
+};
+
+var hold = function() {
+  endTurn();
+  clearDice();
+};
+
+//COMPUTER LOGIC
+var rollDiceComputer = function() {
+  hideOne();
+  if (thrownDice[0] === "2Dice") {
+    dieOne = dice();
+    dieTwo = dice();
+  } else {
+    dieOne = dice();
+    dieTwo = 0;
+  }
+  if (dieOne === 1 || dieTwo === 1) {
+    turnScore = 0;
+    endTurn();
+    computerDiceThrow();
     toggleOff();
     delayClearDice();
     computerTurn1();
   } else {
-    turnScore = turnScore + diceRoll;
-    showDice();
+    turnScore = turnScore + dieOne + dieTwo;
+    computerDiceThrow();
   }
-}
-
-var compHold = function() {
-
-}
+};
 
 var computerTurn1 = function() {
   setTimeout(function() {
-    dice();
+    if (thrownDice[0] === "2Dice"){
+      dieOne = dice();
+      dieTwo = dice();
+    } else {
+      dieOne = dice();
+      dieTwo = 0;
+    }
     computerDiceThrow();
-    if (diceLog[diceLog.length-1] === 1) {
+    if (dieOne === 1 || dieTwo === 1) {
+      turnScore = 0;
       endTurn();
       toggleOn();
     } else {
-      turnScore = turnScore + diceLog[diceLog.length-1];
+      turnScore = turnScore + dieOne + dieTwo;
       if (turnScore >= 12) {
         endTurn();
         diceLog.push(1);
@@ -120,26 +148,30 @@ var computerTurn1 = function() {
         computerTurn1();
       }
     }
-  }, 2000);
-}
+  }, 1500);
+};
+
 
 // user interface logic
 
 var toggleOff = function() {
   $(".userButton").hide();
-}
+};
 
 var toggleOn = function() {
   $(".userButton").show();
-}
+};
 
 var computerDiceThrow = function () {
-  currentRoll = diceLog[diceLog.length-1];
-  $(".dice").append(diceFaces[currentRoll-1]);
-}
-
-
-
+  if (thrownDice[0] === "2Dice"){
+    currentRoll = diceLog[diceLog.length-1];
+    priorRoll = diceLog[diceLog.length-2];
+    $(".dice").append(diceFaces[priorRoll-1] + diceFaces[currentRoll-1])
+  } else {
+    currentRoll = diceLog[diceLog.length-1];
+    $(".dice").append(diceFaces[currentRoll-1]);
+  }
+};
 
 var diceFaces = ['<img src="img/one.png" alt="Die Face">','<img src="img/two.png" alt="Die Face">', '<img src="img/three.png" alt="Die Face">', '<img src="img/four.png" alt="Die Face">', '<img src="img/five.png" alt="Die Face">', '<img src="img/six.png" alt="Die Face">']
 
@@ -150,38 +182,50 @@ var win = function(finalNumber) {
     $(".winner").text(activePlayer());
   } else {
   }
-}
+};
 
 var playerTurnMessage = function(name) {
   $("#currentPlayer").text(name + "'s turn");
   $("#firstPlayer").hide();
-}
+};
 
 var hideOne = function () {
-  if (diceLog[diceLog.length-2] === 1) {
-    $(".dice").text("");
+  if (thrownDice[0] === "2Dice") {
+    if (diceLog[diceLog.length-1] === 1 || diceLog[diceLog.length-2] === 1) {
+      $(".dice").text("");
+    }
+  } else {
+    if (diceLog[diceLog.length-1] === 1) {
+      $(".dice").text("");
+    }
   }
-}
-
-var showDice = function () {
-  currentRoll = diceLog[diceLog.length-1];
-  $(".dice").append(diceFaces[currentRoll-1]);
-}
+};
 
 var clearDice = function() {
   $(".dice").text("");
-}
+};
 
 var delayClearDice = function() {
   setTimeout(function() {
     $(".dice").text("");
-  }, 1900);
+  }, 1400);
+};
+
+var checkDiceUsed = function () {
+  if (thrownDice[0] === "2Dice"){
+    $("#playerRoll1").hide();
+  } else if (thrownDice[0] === "1Die") {
+    $("#playerRoll2").hide();
+  } else { //STRICTLY FOR TEST PURPOSES
+    $("#playerRoll2").hide();
+    $("#playerRoll1").hide();
+  }
 }
 
 var displayScores = function () {
   $(".player1Score").text("Score:" + player1Score());
   $(".player2Score").text("Score:" + player2Score());
-}
+};
 
 $(document).ready(function() {
   // 2Player mode
@@ -189,66 +233,80 @@ $(document).ready(function() {
     event.preventDefault();
     $(".form-group").show();
     $(".playDecision").hide();
+    thrownDice.push($('input[name=diceSelect]:checked').val());
+    console.log(thrownDice);
   });
 
   $(".form-group").submit(function (event) {
     event.preventDefault();
-   $(".form-group").hide();
-   $(".rollAndHold").show();
-
+    $(".form-group").hide();
+    $(".rollAndHold").show();
     player1Name = $("input.player1").val();
     player2Name = $("input.player2").val();
     $("#currentPlayer").text(player1Name + "'s turn");
     currentPlayer.push(player1Name);
     currentPlayer.push(player2Name);
-
     $("h2#player1").text(player1Name);
     $("h2#player2").text(player2Name);
+    checkDiceUsed();
 
-    $("#playerRoll").click(function(event) {
+    $("#playerRoll1").click(function(event) {
       event.preventDefault();
       var currentRoll = dice();
       rollDice(currentRoll);
       console.log(currentRoll);
+      console.log(Scores);
       $(".dice").append(diceFaces[currentRoll-1]);
-
       displayScores();
+    });
 
+    $("#playerRoll2").click(function(event) {
+      event.preventDefault();
+      var firstRoll = dice();
+      var secondRoll = dice();
+      console.log(firstRoll);
+      console.log(secondRoll);
+      roll2Dice(firstRoll, secondRoll);
+      $(".dice").append(diceFaces[(firstRoll - 1)]);
+      $(".dice").append(diceFaces[(secondRoll - 1)]);
+      displayScores();
     });
 
     $("#hold").click(function(event) {
       event.preventDefault();
       hold();
       displayScores();
+      console.log(Scores);
     });
-
   });
+
   //VS Computer Mode
   $("#vsComputer").click(function(event) {
     event.preventDefault();
     $(".computer-form").show();
     $(".playDecision").hide();
-
+    thrownDice.push($('input[name=diceSelect]:checked').val());
+    console.log(thrownDice);
   });
-// --------------------------------------------------
+
   $(".computer-form").submit(function (event) {
     event.preventDefault();
     $(".computer-form").hide();
     $(".rollAndHold").show();
-
     player1Name = $("input.player1").val();
     player2Name = $("input.computer").val();
     $("#currentPlayer").text(player1Name + "'s turn");
     currentPlayer.push(player1Name);
     currentPlayer.push(player2Name);
-// edit for computer play
     $("h2#player1").text(player1Name);
     $("h2#player2").text(player2Name);
-    $("#playerRoll").click(function(event) {
+    $("#playerRoll2").remove();
+
+    $("#playerRoll1").click(function(event) {
       event.preventDefault();
-      var currentRoll = dice();
-      rollDiceComputer(currentRoll);
+      rollDiceComputer();
     });
+
     $("#hold").click(function(event) {
       event.preventDefault();
       hold();
